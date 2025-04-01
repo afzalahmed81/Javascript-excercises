bfe881981d3d2298983c4e962a7d88b507f87c1b
Afzal Ahmed <afzalahmed123@gmail.com>
2025-04-01 20:25:11 +0500

    Pushing Exercise 29,generate a range of numbers

---
+++ numberRange.js
@@ -0,0 +1,16 @@
+// --------Practice 29, function that generates a range of numbers ------
+
+const numRange = (start, end,arr=[]) => {
+    for (let i = start ; i <= end ; i++) {
+        arr.push(i);
+    }
+
+    // Method 2 , using while loop
+    // while (start <= end) {
+    //     arr.push(start);
+    //     start ++ ;
+    // }
+    return arr.join(" , ") ;
+}
+
+console.log(numRange(0,6));
