1c3685a0861d44e2eb3175daf45a7c158e2a0d5b
afzalahmed81 <afzalahmed123@gmail.com>
2025-05-19 10:07:06 +0500

    Exercise 37, function that calculates simple interest

---
+++ calcInterest.js
@@ -0,0 +1,8 @@
+// --------Practice 37: function that calculates simple interest-------
+
+const calcSimpleInterest = (p, r, t) => {
+    return (p*r*t)/100 ;
+}
+
+
+console.log(calcSimpleInterest(10000,5,3));
