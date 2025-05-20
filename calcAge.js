c0f15e41004f2499b887a0c39dc2e87a3694ad89
afzalahmed81 <afzalahmed123@gmail.com>
2025-05-20 11:08:00 +0500

    Exercise 39,function that calculates age of a person

---
+++ calcAge.js
@@ -0,0 +1,15 @@
+// --------Practice 39: function that calculates age of a Person -------
+
+const calcAge = (birthDate) => {
+    let curDate = new Date();
+    birthDate = new Date(birthDate);
+    let age = curDate.getFullYear() - birthDate.getFullYear() ;
+    let monthDiff = curDate.getMonth() - birthDate.getMonth();
+        if (monthDiff < 0 || monthDiff === 0 && curDate.getDate() < birthDate.getDate()) {
+            agg--;
+        }
+        return age;
+}
+
+
+console.log(calcAge("1985-01-01"),"years");
